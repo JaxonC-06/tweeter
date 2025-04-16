28403bdf12c304a3da6fbc3db18a43905a6af7b5
JaxonC-06 <jaxoncostain@outlook.com>
2025-04-16 11:29:17 -0600

    Timeago used to forma timestamp, ajax used to fetch tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
  * @returns {string} - The tweet formatted in HTML
  */
   const createTweetElement = function(tweet) {
+    const tweetDate = timeago.format(tweet.created_at);
     const $tweet = $(`
       <article class="tweet">
         <header>
@@ -22,7 +23,7 @@ $(document).ready(function() {
         </header>
         <p>${tweet.content.text}</p>
         <footer>
-          <p>${tweet.created_at}</p>
+          <p>${tweetDate}</p>
           <div>
             <i class="fa-solid fa-flag tweet-reactions"></i>
             <i class="fa-solid fa-retweet tweet-reactions"></i>
@@ -43,34 +44,6 @@ $(document).ready(function() {
     }
   };
 
-  // Hard-coded initial-tweets data
-  const tweetsData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  renderTweets(tweetsData);
-
   // Form submission w/ jQuery
   $('form').on('submit', function(event) {
     $.ajax({
@@ -84,4 +57,16 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
+
+  // Fetching tweets w/ AJAX
+  const loadTweets = function() {
+    $.ajax({
+      url: "/api/tweets",
+      success: (data) => {
+        renderTweets(data);
+      }
+    });
+  };
+
+  loadTweets();
 });
